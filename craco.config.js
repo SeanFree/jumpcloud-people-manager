93777295765f4ea8b96eb8f366ea0fcf3c545d9e
Sean Free <seanalexanderfree@gmail.com>
2021-08-14 14:04:35 -0400

    Install deps + add craco config

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  style: {
+    sass: {
+      mode: 'extends',
+      loaderOptions: (sassLoaderOptions) => ({
+        ...sassLoaderOptions,
+        additionalData: `@import "styles/global.scss";`
+      })
+    }
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve('./src')
+    }
+  }
+};
